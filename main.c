05f45d5601b68690d2ca1a8a8b5e2814db00c3ae
mercadoMatiasC <pinkumurasakilipsen@gmail.com>
2026-09-05 20:57:31 -0300

    Verificación de lectura de cabecera

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h> //INCLUYE EL TIPO uint8_t
+#include <stdint.h>   //INCLUYE EL TIPO uint8_t
 #include <string.h>
 
 #define RAM_SIZE 16384
@@ -14,21 +14,29 @@ int validarCabecera(char nombre[]);
 //IMPLEMENTACIONES
 int validarCabecera(char nombre[]){
     FILE *archExe = fopen(nombre, "rb");
-    
+
     //CABECERA
     char id[6];
     uint8_t version;
     uint16_t codeSize;
 
     if (archExe){
-        fread(id, 1, 5, archExe);
-        fread(&version, 1, 1, archExe);
-        fread(&codeSize, 1, 2, archExe);
+        fread(id, 5, 1, archExe);
+        fread(&version, sizeof(uint8_t), 1, archExe);
+        fread(&codeSize, sizeof(uint16_t), 1, archExe);
+
+        codeSize = (codeSize << 8) | (codeSize >> 8); //CAMBIO DE CODIFICACION
 
         if (!strcmp(id, IDENTIFICADOR)){
-            printf("ARCHIVO VALIDO!: ID: %s | VER: %d | SIZE: %d");
+            printf("ARCHIVO VALIDO!: ID: %s | VER:  %u | SIZE:  %u", id, version, codeSize);
             return 1;
         }else
             return 0;
     }
+}
+
+int main(){
+    int respuesta = validarCabecera("main.vmx");
+
+    return 0;
 }
