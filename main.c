1c91553ef6527d3119d867a1a677e947954c9c43
valen <valensgorlo@gmail.com>
2026-09-10 16:20:23 -0300

    Definicion de tabla de segmentos primitiva

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 #include <stdint.h>   //INCLUYE EL TIPO uint8_t
 #include <string.h>
 
-#define TAMCABECERA 8
-#define RAM_SIZE 16384
-#define IDENTIFICADOR "VMX26"
+#define TAM_CABECERA 8
+#define CANT_SEGMENTOS 8
+#define TAM_RAM 16384
+#define ID "VMX26"
 #define VERSION 1
 
 
 
 //DEFINICIONES
 int validarCabecera(uint8_t cabecera[]);
+void iniciarTablaSegmentos(uint3)
 //IMPLEMENTACIONES
 int validarCabecera(uint8_t cabecera[]){
 
@@ -21,7 +23,7 @@ int validarCabecera(uint8_t cabecera[]){
     memcpy(id,cabecera,5); //copia al vector id los primeros 5 bytes de cabecera
     id[5]='\0'; 
 
-    if (!strcmp(id, IDENTIFICADOR) && version==VERSION){
+    if (!strcmp(id, ID) && version==VERSION){
         return 1;
     }else
         return 0;
@@ -29,16 +31,17 @@ int validarCabecera(uint8_t cabecera[]){
 
 int main(){
     FILE *archExe = fopen("main.vmx", "rb");
-    uint8_t mainMemory[RAM_SIZE];
-    uint8_t cabecera[TAMCABECERA]; //vector de 8 bytes
-    uint16_t codesize;
+    uint8_t memoriaPrincipal[TAM_RAM];
+    uint8_t cabecera[TAM_CABECERA]; //vector de 8 bytes
+    uint32_t segTabla[CANT_SEGMENTOS]
+    uint16_t tamCodigo;
 
     if(archExe){
         fread(cabecera, sizeof(uint8_t), 8,archExe);
         if(validarCabecera(cabecera)){
-            uint16_t codeSize= ( (uint16_t)cabecera[6]<<8) | cabecera[7];
-            fread(mainMemory,sizeof(uint8_t),codesize,archExe);
-            printf("%x\n",mainMemory[0]);
+            tamCodigo = ( (uint16_t)cabecera[6]<<8) | cabecera[7];
+            fread(memoriaPrincipal,sizeof(uint8_t),tamCodigo,archExe);
+            printf("%x\n",mainMemory[tamCodigo-1]);
             //fread
         }
     }
