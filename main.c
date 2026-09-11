74bb77127f80716323aa2f4bbe314245f9e4dc31
valen <valensgorlo@gmail.com>
2026-09-11 07:59:33 -0300

    Defini cabeceras de las funciones

--- main.c
+++ main.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <stdint.h>   //INCLUYE EL TIPO uint8_t
 #include <string.h>
+#include "operaciones.h"
 
+#define CANT_INSTRUCCIONES 26 
 #define TAM_CABECERA 8
 #define CANT_SEGMENTOS 8
 #define TAM_RAM 16384
 #define ID "VMX26"
 #define VERSION 1
-#define CANT_REGISTROS
+#define CANT_REGISTROS 17
 
 
 
 //DEFINICIONES
 int validarCabecera(uint8_t cabecera[]);
-void iniciarTablaSegmentos(uint32_t tabla[],uint16_t tamCodigo);
+void iniciarTablaSegmentos(int32_t tabla[],uint16_t tamCodigo);
+void iniciarRegTabla(int32_t regTabla[]);
 
 
 //IMPLEMENTACIONES
@@ -32,21 +35,41 @@ int validarCabecera(uint8_t cabecera[]){
         return 0;
 }
 
+
 int main(){
-    FILE *archExe = fopen("main.vmx", "rb");
-    uint8_t memoriaPrincipal[TAM_RAM];
+    FILE *archExe = fopen("ejemplo.vmx", "rb");
+
+    //inst vecInstrucciones[CANT_INSTRUCCIONES]={mov};
+
+    int8_t memoriaPrincipal[TAM_RAM];
     uint8_t cabecera[TAM_CABECERA]; //vector de 8 bytes
-    uint32_t segTabla[CANT_SEGMENTOS]
+    int8_t instruccion;
+    uint8_t codOp,tipoOp1,tipoOp2,tamInstruccion;
+    uint32_t Op1,Op2;
+    uint32_t segTabla[CANT_SEGMENTOS];
     uint16_t tamCodigo;
-    uint32_t regTabla[CANT_REGISTROS]
+    int32_t regTabla[CANT_REGISTROS];
+
+    int i;
 
     if(archExe){
         fread(cabecera, sizeof(uint8_t), 8,archExe);
         if(validarCabecera(cabecera)){
             tamCodigo = ( (uint16_t)cabecera[6]<<8) | cabecera[7];
             fread(memoriaPrincipal,sizeof(uint8_t),tamCodigo,archExe);
-            /*Para recorrer todo el codigo while(!(ip<tamCodigo y ip!=-1)) o parecido*/
+            
+            
+            
+                
+                
+                
+                
+
+            
+
+
         }
+        fclose(archExe);
     }
     
 
--- operaciones.c
+++ operaciones.c
@@ -4,29 +4,6 @@
 
 
 
-typedef void op2operandos(int16_t *,int16_t *);
 
-void mov(int16_t *a,int16_t *b){
-    *a=*b;
-}
-void add(int16_t *a,int16_t *b){
-    *a+=*b;
-}
-void sub(int16_t *a,int16_t *b){
-    *a-=*b;
-}
-int esNegativo(int16_t a){
-    return (a&(0x8000))>>15 & 0x0001; //seria mejor comparar con !=0 en lugar de 0x0001?
-}
-int esCero(int16_t a){
-    return a==0;
-}
-int esPositivo(int16_t a){
-    return !esNegativo(a) && !esCero(a);
-}
-int igualSigno(int16_t a,int16_t b){
-    return esNegativo(a)==esNegativo(b);
-}
-int esDesbordamiento(int16_t a,int16_t b,int16_t res){
-    return igualSigno(a,b) && !igualSigno(a,res) ;
-}
+
+
